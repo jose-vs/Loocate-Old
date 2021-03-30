4d3f8e1378b76c3fdd623aaacddb3f07ebf89697
David <davidbrettandersonnz@gmail.com>
2021-03-31 00:37:55 +1300

    added firebase config
    requires firebase dependency

--- App.js
+++ App.js
@@ -2,6 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import MapScreen from './src/screens/MapScreen';
+import ApiKeys from './constants/ApiKeys';
+import * as firebase from 'firebase'
+
+//initalize Firebase 
+if (!firebase.apps.length) { //Checks if firebase has already been initialized
+  firebase.initializeApp(ApiKeys.FirebaseConfig);
+}
 
 const navigator = createStackNavigator(
   {
---
+++ constants/ApiKeys.js
@@ -0,0 +1,12 @@
+export default{
+    FirebaseConfig: {
+        apiKey: "AIzaSyD_LZ9-kz0CUGPMI2zP4jtlks2Gd1t5xLs",
+        authDomain: "loocate-74bb1.firebaseapp.com",
+        databaseURL: "https://loocate-74bb1-default-rtdb.firebaseio.com",
+        projectId: "loocate-74bb1",
+        storageBucket: "loocate-74bb1.appspot.com",
+        messagingSenderId: "858712654924",
+        appId: "1:858712654924:web:636f55794c6c60409920e9",
+        measurementId: "G-RG56NPXTR1"
+    },
+}
