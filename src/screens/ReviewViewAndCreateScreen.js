dbd7ed3c1fd60dcec06f9f5f178e67692904fc94
castehlin <castehlin@gmail.com>
2021-05-25 23:35:59 +1200

    View reviews/submit reviews fully functional and displays with no issue. List screen toilet cards also now direct to the view reviews screen. The next step is moving changes to the bottomscreen.

--- src/screens/ReviewViewAndCreateScreen.js
+++ src/screens/ReviewViewAndCreateScreen.js
@@ -8,45 +8,49 @@ import { StackActions } from '@react-navigation/native';
 
 export default function ReviewViewAndCreateScreen({ route, navigation }) {
   
-  let userInput = ('');
-  let review = ('');
-  const [existingReviewArray, setExistingReviewArray] = useState([]);
+  var review = ('');
+  var addToReviewsArray = [];
+  const [existingReviewsArray, setExistingReviewsArray] = useState([]);
+  const usersRef = firebase.firestore().collection("users"); 
   const reviewsRef = firebase.firestore().collection('reviews');
   const popAction = StackActions.pop(1); //go back one screen in stack (number represents how many screens to go back by)
 
-      //fetches reviews from database and puts them in local array
+    //fetches reviews from database and puts them in local array
     useEffect(() => {
       reviewsRef.get().then((querySnapshot) => {
         querySnapshot.forEach(snapshot => {
             if (snapshot.data().toiletID == route.params.id){
               var existingReview = snapshot.data(); 
-              setExistingReviewArray([...existingReviewArray, existingReview]);
-              console.log(existingReviewarray);
+              addToReviewsArray = ([...addToReviewsArray , existingReview]);          
             } 
         }
-      )
+      )    
+      setExistingReviewsArray(addToReviewsArray);
     });
     }, []); 
 
     //Submit review on selected toilet if logged in. If not logged in, alert and do nothing.
     const onSubmitReviewPress = () => {      
         firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-          const usersRef = firebase.firestore().collection("users"); 
+        if (user) {       
           usersRef
           .doc(user.uid)
           .get()
           .then((document) => {
           const data = document.data();
-        
+
           reviewsRef.add({
             title: review,
             address: route.params.address,
             toiletID: route.params.id,
             userID: data.id,
-            rating: 0 //add in star rating variable when I get around to it...maybe just reference the one from toilets, but that is google...hmm.
+            rating: route.params.loocateRating,  //add in star rating variable when I get around to it...maybe just reference the one from toilets, but that is google...hmm.
             })
+            
+          setExistingReviewsArray([...existingReviewsArray , {title: review, address: route.params.address, toiletID: route.params.id, 
+            userID: data.id, rating: route.params.loocateRating}]); 
         })
+
         Alert.alert(
           'Submission success',
           'Your review has been placed.'); 
@@ -64,11 +68,10 @@ export default function ReviewViewAndCreateScreen({ route, navigation }) {
      * so that they can be rendered in the useState.
     */
     const onViewReviewPress = () => { 
-        if (existingReviewArray.length) {
-          console.log(existingReviewArray);
-          navigation.navigate('DisplayReviews', existingReviewArray);         
+        if (existingReviewsArray.length) {
+          navigation.navigate('DisplayReviews', existingReviewsArray);         
         }   
-        else if (!existingReviewArray.length) {
+        else if (!existingReviewsArray.length) {
           Alert.alert (
             'No reviews found.',
             'Be the first to create a review for this toilet!');
